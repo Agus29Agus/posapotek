1f033d30a44c9e23944c642f843b1a0d43a8f4bf
Agus29Agus <97592801+Agus29Agus@users.noreply.github.com>
2023-05-18 18:35:37 +0700

    Delete  Href header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="dashboard" class="logo">
+    <a class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>POS</b></span>
         <!-- logo for regular state and mobile devices -->
